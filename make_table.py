25ceeeaa9a30a0f5729ea029b5d08fd9de0e224f
Serafim Nenarokov <serafim221@gmail.com>
2022-11-28 16:19:03 +0100

    Update code and table2asn version

--- make_table.py
+++ make_table.py
@@ -7,7 +7,9 @@ GENES_FILENAME = 'genes.fasta'
 FUNC_ANNOT_FILENAME = 'func_annot.tsv'
 STRUCT_ANNOT_FILENAME = 'struct_annot.gff'
 RNA_ANNOT_FILENAME = 't_r_rnas.gff'
-OUTPUT_PATH = 'final/LWC14.tbl'
+OUTPUT_PATH = 'final/result.gff'
+# OUTPUT_PATH = 'final/LWC14.tbl'
+
 LOCUS_PREFIX = 'LWC14'
 DEFAULT_FUNCTION = 'hypothetical protein'
 
@@ -50,6 +52,91 @@ def make_table_entry(gene_obj):
     return result
 
 
+def dict_to_gff_description(d):
+    return ';'.join([f"{k}={v}" for k, v in d.items()])
+
+
+def join_gff_list(l):
+    return '\t'.join([str(e) for e in l])
+
+def make_gff_entry(gene_obj):
+    # {
+    # 'contig_id': 'Ctg44_length_309375',
+    # 'contig_length': 309375,
+    # 'coords': [118692, 120407],
+    # 'direction': '+',
+    # 'frame': 2,
+    # 'gene_func': 'Rieske-like [2Fe-2S] domain containing protein, putative',
+    # 'gene_name': '47b2ceaaed138458aded4cc06e4178bc',
+    # 'locus_tag': 'LWC14_00001',
+    # 'source': 'annotation',
+    # 'type': 'gene',
+    # 'zoi_coords': [118434, 121751]
+    # }
+
+    rows = []
+
+    source = 'nonstop_annotator'
+    product = gene_obj['gene_func'] if gene_obj['gene_func'] else DEFAULT_FUNCTION
+
+    # 1. gene (ID=Pelo_1;locus_tag=Pelo_1)
+    record = [gene_obj['contig_id'], source, 'gene']
+    record.extend(gene_obj['zoi_coords'])
+    record.extend(['.', gene_obj['direction'], gene_obj['frame']])
+
+    gene_id = gene_obj['locus_tag']
+    description = {'ID': gene_id, 'locus_tag': gene_id}
+    record.append(dict_to_gff_description(description))
+    rows.append(join_gff_list(record))
+
+
+    # 2. mRNA
+    record = [gene_obj['contig_id'], source, 'mRNA']
+    record.extend(gene_obj['zoi_coords'])
+    record.extend(['.', gene_obj['direction'], gene_obj['frame']])
+
+    mrna_id = f"{gene_id}_1"
+    description = {'ID': mrna_id, 'Parent': gene_id, 'product': product}
+    record.append(dict_to_gff_description(description))
+    rows.append(join_gff_list(record))
+
+
+    # 3. exon
+    record = [gene_obj['contig_id'], source, 'exon']
+    record.extend(gene_obj['coords'])
+    record.extend(['.', gene_obj['direction'], gene_obj['frame']])
+
+    exon_id = f"{gene_id}.exon1"
+    description = {'ID': exon_id, 'Parent': mrna_id, 'product': product}
+    record.append(dict_to_gff_description(description))
+    rows.append(join_gff_list(record))
+
+    # 4. CDS
+    record = [gene_obj['contig_id'], source, 'CDS']
+    record.extend(gene_obj['coords'])
+    record.extend(['.', gene_obj['direction'], gene_obj['frame']])
+
+    cds_id = f"cds.{gene_id}"
+    description = {'ID': cds_id, 'Parent': mrna_id, 'product': product}
+    record.append(dict_to_gff_description(description))
+    rows.append(join_gff_list(record))
+
+
+    # [print(e + "\n") for e in rows]
+
+    result = '\n'.join(rows)
+    result += '\n'
+
+    return result
+
+
+def parse_struct_annotation_description(raw_field):
+    description = raw_field.strip().split(';')
+    description = [[e.split('=')[0], e.split('=')[1]] for e in description]
+    description = dict(description)
+    return description
+
+
 def main():
     annotated_genes = {}
     struct_annot_data = {}
@@ -68,6 +155,15 @@ def main():
             if key not in struct_annot_data:
                 struct_annot_data[key] = {}
             struct_annot_data[key]['direction'] = splitted[6].strip()
+
+            description = parse_struct_annotation_description(splitted[8].strip())
+
+            struct_annot_data[key]['zoi_start'] = description['zoi_start']
+            struct_annot_data[key]['zoi_finish'] = description['zoi_finish']
+
+            struct_annot_data[key]['contig_id'] = splitted[0].strip()
+            struct_annot_data[key]['frame'] = splitted[7].strip()
+
 
     # Preparing predicted genes, fetch data from functional annotation
 
@@ -97,6 +193,12 @@ def main():
                 raise(Exception(f'Cannot find coordinates {key_coords} in structural annotation'))
             else:
                 gene_obj['direction'] = struct_annot_data[key_coords]['direction']
+                zoi_coords = []
+                zoi_coords.append(struct_annot_data[key_coords]['zoi_start'])
+                zoi_coords.append(struct_annot_data[key_coords]['zoi_finish'])
+                gene_obj['zoi_coords'] = [int(e) for e in zoi_coords]
+                gene_obj['contig_id'] = struct_annot_data[key_coords]['contig_id']
+                gene_obj['frame'] = struct_annot_data[key_coords]['frame']
 
             contig_length = int(contig_id.split('_')[-1])
             gene_obj['contig_length'] = contig_length
@@ -113,6 +215,7 @@ def main():
             gene_obj['source'] = 'annotation'
 
             annotated_genes[contig_id].append(gene_obj)
+
             gene_cnt += 1
 
     # preparing RNAs
@@ -121,6 +224,7 @@ def main():
         cnt = 1
 
         for line in f:
+            break
             line = line.strip()
             splitted = line.split('\t')
 
@@ -157,9 +261,12 @@ def main():
 
     with open(OUTPUT_PATH, 'w') as out_f:
         for contig_id, data in annotated_genes.items():
-            out_f.write(make_feautre_header(contig_id))
+            # >Feature	Ctg44_length_309375
+            # out_f.write(make_feautre_header(contig_id))
+
             for gene_obj in data:
-                out_f.write(make_table_entry(gene_obj))
+                # out_f.write(make_table_entry(gene_obj))
+                out_f.write(make_gff_entry(gene_obj))
 
 
 if __name__ == '__main__':
